45bb7cc58a728b11f2b3bb6d83eee07aff6b6118
obenali <oussama.benali@ump.ac.ma>
2026-03-30 18:12:48 +0100

    update

--- json-researcher.sh
+++ json-researcher.sh
@@ -1,1 +1,1 @@
-curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq '.[] | select(.id == 1)' | grep -E '^\s+"name"|^\s+"power"'
+curl  -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq '.[] | select(.id == 1)' | grep -E '^\s+"name"|^\s+"power"'
