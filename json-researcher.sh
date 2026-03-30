ff636b0f2a125cc52b4c7cc1ad63357e39aff773
obenali <oussama.benali@ump.ac.ma>
2026-03-30 14:31:54 +0100

    more regix

--- json-researcher.sh
+++ json-researcher.sh
@@ -1,1 +1,1 @@
-curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq '.[] | select(.id == 1)' | grep -E '"name"|"power"'
+curl -s https://learn.zone01oujda.ma/assets/superhero/all.json | jq '.[] | select(.id == 1)' | grep -E '^\s+"name"|^\s+"power"'
