d3b50987e9488f61ac7f3caf2e3878cebf8ef3e7
obenali <oussama.benali@ump.ac.ma>
2026-04-01 11:35:39 +0100

    talked

---
+++ easy-conditions.sh
@@ -0,0 +1,9 @@
+#export X=29
+#export Y=12
+
+#!/bin/bash
+if [ $X -gt $Y ]; then
+    echo "true"
+else
+    echo "false"
+fi
---
+++ show-info.sh
@@ -0,0 +1,5 @@
+cat -e << INNER
+The current directory is: /home/obenali/piscine-scripting
+The default paths are: /home/obenali/.local/bin:/home/obenali/.docker/cli-plugins:/home/obenali/bin:/home/obenali/.docker/cli-plugins:/home/obenali/bin:/home/obenali/.docker/cli-plugins:/home/obenali/bin:/home/obenali/.docker/cli-plugins:/home/obenali/bin:/home/obenali/.docker/cli-plugins:/home/obenali/bin:/home/obenali/.docker/cli-plugins:/home/obenali/bin:/home/obenali/bin:/home/obenali/.cargo/bin:/home/obenali/.local/bin:/home/obenali/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin:/opt/flutter/bin:/usr/local/go/bin
+The current user is: obenali
+INNER
