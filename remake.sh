550b4ab95e02bf16445af0eeb3584a4188f38f5b
obenali <oussama.benali@ump.ac.ma>
2026-04-04 11:48:15 +0100

    2025

--- remake.sh
+++ remake.sh
@@ -7,28 +7,26 @@ fi
 
 dir=$1
 
-
-
 touch "$dir/ciao"
 chmod 442 "$dir/ciao"
-touch -t 202401010001 "$dir/ciao"
+touch -t 202601010001 "$dir/ciao"
 
 mkdir -p "$dir/mamma"
 chmod 777 "$dir/mamma"
-touch -t 202401020001 "$dir/mamma"
+touch -t 202601020001 "$dir/mamma"
 
 touch "$dir/guarda"
 chmod 400 "$dir/guarda"
-touch -t 202401030001 "$dir/guarda"
+touch -t 202601030001 "$dir/guarda"
 
 touch "$dir/come"
 chmod 642 "$dir/come"
-touch -t 202401040001 "$dir/come"
+touch -t 202601040001 "$dir/come"
 
 mkdir -p "$dir/mi"
 chmod 452 "$dir/mi"
-touch -t 202401050001 "$dir/mi"
+touch -t 202601050001 "$dir/mi"
 
 touch "$dir/diverto"
 chmod 421 "$dir/diverto"
-touch -t 202401060001 "$dir/diverto"
+touch -t 202601060001 "$dir/diverto"
