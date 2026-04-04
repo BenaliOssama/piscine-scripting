4de9032233e4640e4c9964d81b27dfa46b99dbe0
obenali <oussama.benali@ump.ac.ma>
2026-04-04 11:41:23 +0100

    vfx

---
+++ remake.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]] || ! [[ -d $1 ]]; then
+    echo "Error"
+    exit 0
+fi
+
+dir=$1
+
+
+
+touch "$dir/ciao"
+chmod 442 "$dir/ciao"
+touch -t 202401010001 "$dir/ciao"
+
+mkdir -p "$dir/mamma"
+chmod 777 "$dir/mamma"
+touch -t 202401020001 "$dir/mamma"
+
+touch "$dir/guarda"
+chmod 400 "$dir/guarda"
+touch -t 202401030001 "$dir/guarda"
+
+touch "$dir/come"
+chmod 642 "$dir/come"
+touch -t 202401040001 "$dir/come"
+
+mkdir -p "$dir/mi"
+chmod 452 "$dir/mi"
+touch -t 202401050001 "$dir/mi"
+
+touch "$dir/diverto"
+chmod 421 "$dir/diverto"
+touch -t 202401060001 "$dir/diverto"
