895c1285a64c888d1dc621fa9e20abba8b215cc8
obenali <oussama.benali@ump.ac.ma>
2026-04-04 16:56:34 +0100

    failled agian the test

--- grades.sh
+++ grades.sh
@@ -9,10 +9,10 @@ declare -a names
 declare -a grades
 
 for i in $(seq 1 $1); do
-    read name
+    read -p "Student Name #$i: " name
     names+=("$name")
 
-    read grade
+    read -p "Student Grade #$i: " grade
 
     if ! [[ $grade =~ ^[0-9]+$ ]] || [[ $grade -gt 100 ]]; then
         echo "Error: The grade '$grade' is not a valid input. Only numerical grades between 0 and 100 are accepted." >&2
@@ -24,3 +24,15 @@ done
 
 for i in $(seq 0 $(( $1 - 1 ))); do
     name=${names[$i]}
+    grade=${grades[$i]}
+
+    if [[ $grade -ge 90 ]]; then
+        echo "$name: You did an excellent job!"
+    elif [[ $grade -ge 70 ]]; then
+        echo "$name: You did a good job!"
+    elif [[ $grade -ge 50 ]]; then
+        echo "$name: You need a bit more effort!"
+    else
+        echo "$name: You had a poor performance!"
+    fi
+done
