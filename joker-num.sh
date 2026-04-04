549ba50941a7735bcc81af3309e862a59ad9a719
obenali <oussama.benali@ump.ac.ma>
2026-04-04 14:30:13 +0100

    ?

--- joker-num.sh
+++ joker-num.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]; then
-    echo "Error: wrong argument"
-    exit 1
-fi
+# if [[ $# -ne 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]; then
+#     echo "Error: wrong argument"
+#     exit 1
+# fi
 if [[ $# -ne 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]] || [[ $1 -lt 1 ]] || [[ $1 -gt 100 ]]; then
     echo "Error: wrong argument"
     exit 1
