3bdd59904bb92526df5ddf93e4e45674e645abf0
obenali <oussama.benali@ump.ac.ma>
2026-04-04 14:22:02 +0100

    space makes a difference

--- joker-num.sh
+++ joker-num.sh
@@ -10,7 +10,7 @@ moves=0
 tries_left=5
 
 while [[ $tries_left -gt 0 ]]; do
-    echo "Enter your guess ($tries_left tries left): "
+    echo "Enter your guess ($tries_left tries left):"
     read guess
 
     if [[ -z "$guess" ]] || ! [[ $guess =~ ^[0-9]+$ ]] || [[ $guess -lt 1 ]] || [[ $guess -gt 100 ]]; then
