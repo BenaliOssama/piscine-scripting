802246e26cc1639ef2c5d9d0dc7181b66b425070
obenali <oussama.benali@ump.ac.ma>
2026-04-04 11:29:26 +0100

    add division

---
+++ division.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ $# -lt 2 ]]; then
+	echo "Error: two numbers must be provided"
+	exit 1
+fi
+
+if ! [[ $1 =~ ^-?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]+$ ]] ; then 
+	echo "Error: both arguments must be integers"
+	exit 1 
+fi
+
+
+if [[ $2 -eq 0 ]]; then 
+	echo "Error: division by zero is not allowed"
+	exit 1j
+fi
+
+echo "$1 / $2" | bc
