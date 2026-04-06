23c270b5008634600a448e2a0333b7dfe0d5b088
obenali <oussama.benali@ump.ac.ma>
2026-04-06 12:57:29 +0100

    p

--- punishment.py
+++ punishment.py
@@ -1,3 +1,5 @@
 def do_punishment(first_part, second_part, nb_lines):
     sentence = first_part + " " + second_part + "."
+    if nb_lines == 0:
+        return ""
     return "\n".join([sentence] * nb_lines) + "\n"
