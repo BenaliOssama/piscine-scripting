fdd786e231ef0952f36f5c998118e4c07baf1ec2
obenali <oussama.benali@ump.ac.ma>
2026-04-07 20:54:45 +0100

    M

--- skip_secrets.py
+++ skip_secrets.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
-if len(sys.argv) != 2 :
+if len(sys.argv) != 2:
     sys.exit(1)
 
 file_name = sys.argv[1]
 
-if not os.acess(file_name, os.R_OK):
+if not os.access(file_name, os.R_OK):
     sys.exit(1)
 
 with open(file_name, 'r') as f_in:
@@ -16,4 +16,3 @@ with open('out.txt', 'w') as f_out:
     for line in lines:
         if 'pineapple' not in line:
             f_out.write(line)
-
