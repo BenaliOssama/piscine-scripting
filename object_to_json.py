9b4bab494734bc041c7722282a0fbab975ad48b4
obenali <oussama.benali@ump.ac.ma>
2026-04-07 16:23:59 +0100

    te

---
+++ object_to_json.py
@@ -0,0 +1,32 @@
+import json
+
+class User:
+    username = 'user'
+    email = 'something@mail.com'
+
+
+def create_new_user(data):
+    # validate input
+    if not isinstance(data, dict):
+        return User()
+
+    if "username" not in data or "email" not in data:
+        return User()
+
+
+    user = User()
+    
+    user.username = data["username"]
+    user.email = data["email"]
+
+    return user
+
+
+def user_to_json(user):
+    # convert user object to JSON string
+    user_dict = {
+        "username" : user.username, 
+        "email": user.email
+    }
+
+    return json.dumps(user_dict)
