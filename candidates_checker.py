86386b7c0d15949d03023a12d01bf4f3eb4c7639
obenali <oussama.benali@ump.ac.ma>
2026-04-07 19:39:12 +0100

    under

---
+++ candidates_checker.py
@@ -0,0 +1,26 @@
+import sys 
+
+if __name__ == '__main__': # this file was run directly from the terminal
+    if len(sys.argv) != 2:
+        print("Error: wrong number of arguments")
+        sys.exit(1)
+
+    n = int(sys.argv[1])
+    candidates = []
+
+    for _ in range(n):
+        print("Add a new candidate:")
+        name = input("name: ")
+        age = int(input("age: "))
+        candidates.append({"name": name, "age": age})
+
+    for candidate in candidates:
+        name = candidate["name"]
+        age = candidate["age"]
+
+        if age < 18:
+            print(f"{name} is not eligible (underaged)")
+        elif age > 60: 
+            print(f"{name} is not eligible (over the legal age)")
+        else:
+            print(f"{name} is eligible")
