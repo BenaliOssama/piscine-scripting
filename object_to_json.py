21160e272c28b0847313e6ed91428a0d05594e66
obenali <oussama.benali@ump.ac.ma>
2026-04-07 18:52:49 +0100

    m

--- object_to_json.py
+++ object_to_json.py
@@ -1,33 +1,23 @@
-# object_to_json.py
-
 import json
-
 
 class User:
     username = 'user'
     email = 'something@mail.com'
 
-    def __init__(self, username=None, email=None):
-        if username is not None:
-            self.username = username
-        if email is not None:
-            self.email = email
-
-
-def create_new_user(data):
+def create_new_user(registration_json):
+    default = User()
     try:
-        obj = json.loads(data)
-    except Exception:
-        return User()
-
-    if isinstance(obj, dict) and "username" in obj and "email" in obj:
-        return User(obj["username"], obj["email"])
-
-    return User()
-
+        data = json.loads(registration_json)
+    except (json.JSONDecodeError, TypeError):
+        return default
+    
+    if 'username' not in data or 'email' not in data:
+        return default
+    
+    user = User()
+    user.username = data['username']  # written to the instance's __dict__
+    user.email = data['email']
+    return user
 
 def user_to_json(user):
-    if user.username == "user" and user.email == "something@mail.com":
-        return json.dumps({})
-
     return json.dumps(user.__dict__)
